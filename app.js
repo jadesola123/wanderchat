8a5c9833102b7c1eae095a7624306123f4f8ecde
jadesola kareem <63196957+jadesola123@users.noreply.github.com>
2023-05-23 02:31:50 +0000

    files added

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const sql = require('mssql');
+const http = require('http');
+const path = require('path');
+
+const config = {
+  user: 'wander',
+  password: 'Passworld23$',
+  server: 'wander.database.windows.net',
+  database: 'WanderDb',
+  options: {
+    encrypt: true,
+  },
+};
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/memes', (req, res) => {
+  const query = 'SELECT TOP 1 url, caption FROM Memes ORDER BY NEWID()';
+
+  sql.connect(config)
+    .then((pool) => {
+      return pool.request().query(query);
+    })
+    .then((result) => {
+      const meme = result.recordset[0];
+      res.json(meme);
+    })
+    .catch((err) => {
+      console.error('Failed to retrieve meme:', err);
+      res.status(500).json({ error: 'Failed to retrieve meme' });
+    });
+});
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
